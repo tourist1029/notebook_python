68f73dd4079f48923a76487f3a10901a3c938635
tourist1029 <enricyx@outlook.com>
2019-07-17 18:47:15 +0800

    sixth push bare test

--- head.tcl
+++ head.tcl
@@ -38,6 +38,7 @@ proc PB_CMD_HEAD_INFO2 {} {
     #third push for bare test
     #fourth push for bare test change
     #fifth push for bare test 
+    #sixth push for bare test
     #用于判断是否为T型刀
     set TC "Milling Tool-T Cutter"
     #用于判断是否为钻头
